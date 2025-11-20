2c94ff2dfc646246683abd58c8c938c34b09a8f3
OURO-AKPO Amiir <amiirouroakpo@gmail.com>
2025-11-20 13:48:06 +0000

    OOUps

--- src/Api/axios_instance.ts
+++ src/Api/axios_instance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
+  // baseURL: 'https://your-production-api.com/api',
   baseURL: 'http://127.0.0.1:8000/api',
   timeout: 10000,
   headers: {
