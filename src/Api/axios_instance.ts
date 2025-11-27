72d835215e9568620ded88c96bcb39f5d0d4042b
OURO-AKPO Amiir <amiirouroakpo@gmail.com>
2025-11-27 08:57:31 +0000

    My Last Commit

--- src/Api/axios_instance.ts
+++ src/Api/axios_instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.0.41:8080/Dzumevi_APi/public/api',
+  baseURL: 'http://192.168.0.119:8080/Dzumevi_APi/public/api',
   // baseURL: 'http://127.0.0.1:8000/api',
   timeout: 20000,
   headers: {
